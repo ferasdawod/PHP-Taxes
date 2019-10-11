b4617a5e815ce2a72a4de27c86d4cce3cb8d09f0
Feras Dawod <feras.da94@gmail.com>
2019-10-11 16:57:10 +0200

    Add and implement State interface

--- app/Models/State.php
+++ app/Models/State.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
+use App\Interfaces\IState;
 use Illuminate\Database\Eloquent\Model;
 
-class State extends Model
+class State extends Model implements IState
 {
     public $fillable = [
         'name',
     ];
+
+    function getName(): String {
+        return $this->name;
+    }
 }
