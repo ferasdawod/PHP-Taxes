54cb19552133dd1d4e8460ce02327500f037a8b1
Feras Dawod <feras.da94@gmail.com>
2019-10-13 22:38:33 +0200

    Add stub implementations for csv and db reporting implementations

---
+++ app/Services/Reporting/Implementations/CsvReportingService.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ASUS
+ * Date: 13/10/2019
+ * Time: 10:17 PM
+ */
+
+namespace App\Services\Reporting\Implementations;
+
+
+use App\Services\Reporting\IReportingService;
+
+class CsvReportingService implements IReportingService
+{
+    function getStatesAverageTaxes(): array
+    {
+        // TODO: Implement getStatesAverageTaxes() method.
+    }
+
+    function getStatesTotalTaxes(): array
+    {
+        // TODO: Implement getStatesTotalTaxes() method.
+    }
+
+    function getStatesAverageTaxRate(): array
+    {
+        // TODO: Implement getStatesAverageTaxRate() method.
+    }
+
+    function getAverageTaxRate(): float
+    {
+        // TODO: Implement getAverageTaxRate() method.
+    }
+
+    function getTotalTaxes(): float
+    {
+        // TODO: Implement getTotalTaxes() method.
+    }
+}
