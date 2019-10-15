9bc07e31bd70137ce9567be7601d3d4c51bddc86
Feras Dawod <feras.da94@gmail.com>
2019-10-15 13:39:49 +0200

    Add counties relationship to state model

--- app/Models/State.php
+++ app/Models/State.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use App\Interfaces\IState;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class State extends Model
 {
     public $fillable = [
         'name',
     ];
+
+    public function counties() : HasMany {
+        return $this->hasMany(County::class, 'state_id', 'id');
+    }
 }
