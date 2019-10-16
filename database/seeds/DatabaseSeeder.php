bed45ab05faa57eb53d8bf91e809c7d51e2da005
Feras Dawod <feras.da94@gmail.com>
2019-10-16 21:35:13 +0200

    Decrease count of seeded data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,9 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         factory(\App\Models\State::class, 5)->create()->each(function (\App\Models\State $state) {
-            $state->counties()->saveMany(factory(\App\Models\County::class, rand(2, 10))->make());
+            $state->counties()->saveMany(factory(\App\Models\County::class, rand(1, 5))->make());
             $state->counties->each(function (\App\Models\County $county) {
-                $county->entries()->saveMany(factory(\App\Models\TaxEntry::class, rand(10, 50))->make());
+                $county->entries()->saveMany(factory(\App\Models\TaxEntry::class, rand(2, 10))->make());
             });
         });
     }
