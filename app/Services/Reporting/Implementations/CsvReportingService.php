eb69b3120fe012402a14484e031a24e1236af6c0
Feras Dawod <feras.da94@gmail.com>
2019-10-16 21:54:05 +0200

    Add function to load the datafile

--- app/Services/Reporting/Implementations/CsvReportingService.php
+++ app/Services/Reporting/Implementations/CsvReportingService.php
@@ -6,28 +6,36 @@ use App\Services\Reporting\IReportingService;
 
 class CsvReportingService implements IReportingService
 {
-    function getStatesAverageTaxes(): array
-    {
-        // TODO: Implement getStatesAverageTaxes() method.
-    }
-
-    function getStatesTotalTaxes(): array
+    public function getStatesAverageTaxes(): array
     {
         // TODO: Implement getStatesTotalTaxes() method.
     }
 
-    function getStatesAverageTaxRate(): array
+    public function getStatesTotalTaxes(): array
+    {
+        // TODO: Implement getStatesTotalTaxes() method.
+    }
+
+    public function getStatesAverageTaxRate(): array
     {
         // TODO: Implement getStatesAverageTaxRate() method.
     }
 
-    function getAverageTaxRate(): float
+    public function getAverageTaxRate(): float
     {
         // TODO: Implement getAverageTaxRate() method.
     }
 
-    function getTotalTaxes(): float
+    public function getTotalTaxes(): float
     {
         // TODO: Implement getTotalTaxes() method.
+    }
+
+    public function loadDataFromFile() : array {
+        // load the data file contents and parse the csv format then
+        // return the data without the header row as we are only concerned with the data
+        $fileContents = Storage::get(config('reporting.csv.path'));
+        $data = array_map('str_getcsv', $fileContents);
+        return array_splice($data, 1);
     }
 }
