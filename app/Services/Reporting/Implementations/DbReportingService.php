1c9f07b954f8bde9c3a4d29893ac2ee2193855fd
Feras Dawod <feras.da94@gmail.com>
2019-10-16 21:35:54 +0200

    Implement db reporting service

--- app/Services/Reporting/Implementations/DbReportingService.php
+++ app/Services/Reporting/Implementations/DbReportingService.php
@@ -2,32 +2,69 @@
 
 namespace App\Services\Reporting\Implementations;
 
+use App\Models\County;
+use App\Models\State;
+use App\Models\TaxEntry;
 use App\Services\Reporting\IReportingService;
+use App\ViewModels\Reporting\StateReportUnit;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Facades\DB;
 
 class DbReportingService implements IReportingService
 {
-    function getStatesAverageTaxes(): array
+    public function getStatesAverageTaxes(): array
     {
-        // TODO: Implement getStatesAverageTaxes() method.
+        // Implementation #1 (database query)
+//        $result = DB::table('states')
+//            ->join('counties', 'states.id', '=', 'counties.state_id')
+//            ->join('tax_entries', 'tax_entries.county_id', '=', 'counties.id')
+//            ->selectRaw('states.id, states.name, AVG(tax_entries.amount) as avg_taxes')
+//            ->groupBy('states.id')
+//            ->get()
+//            ->toArray();
+
+        // Implementation #2 (client side)
+        $result = State::query()->with('entries')
+            ->get()
+            ->map(function (State $state) {
+                return new StateReportUnit($state, $state->entries->avg('amount'));
+            })
+            ->all();
+
+        return $result;
     }
 
-    function getStatesTotalTaxes(): array
+    public function getStatesTotalTaxes(): array
     {
-        // TODO: Implement getStatesTotalTaxes() method.
+        $result = State::query()->with('entries')
+            ->get()
+            ->map(function (State $state) {
+                return new StateReportUnit($state, $state->entries->sum('amount'));
+            })
+            ->all();
+
+        return $result;
     }
 
-    function getStatesAverageTaxRate(): array
+    public function getStatesAverageTaxRate(): array
     {
-        // TODO: Implement getStatesAverageTaxRate() method.
+        $result = State::query()->with('counties')
+            ->get()
+            ->map(function (State $state) {
+                return new StateReportUnit($state, $state->counties->avg('tax_rate'));
+            })
+            ->all();
+
+        return $result;
     }
 
-    function getAverageTaxRate(): float
+    public function getAverageTaxRate(): float
     {
-        // TODO: Implement getAverageTaxRate() method.
+        return County::query()->average('tax_rate');
     }
 
-    function getTotalTaxes(): float
+    public function getTotalTaxes(): float
     {
-        // TODO: Implement getTotalTaxes() method.
+        return TaxEntry::query()->sum('amount');
     }
 }
