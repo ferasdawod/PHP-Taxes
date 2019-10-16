986f026f6bd969ccd3f71f3615c4c19e5fe2122a
Feras Dawod <feras.da94@gmail.com>
2019-10-16 22:43:27 +0200

    Finish initial implementation of the csv reporting service

--- app/Services/Reporting/Implementations/CsvReportingService.php
+++ app/Services/Reporting/Implementations/CsvReportingService.php
@@ -3,39 +3,99 @@
 namespace App\Services\Reporting\Implementations;
 
 use App\Services\Reporting\IReportingService;
+use App\ViewModels\Reporting\StateReportUnit;
+use Illuminate\Support\Collection;
 
 class CsvReportingService implements IReportingService
 {
+    /**
+     * @return StateReportUnit[]
+     */
     public function getStatesAverageTaxes(): array
     {
-        // TODO: Implement getStatesTotalTaxes() method.
+        $data = $this->loadDataFromFile();
+        $result = $this->aggregateStates($data, function (Collection $rows) {
+            return $rows->average(function ($row) {
+                return $row[4];
+            });
+        });
+
+        return $result;
     }
 
+    /**
+     * @return StateReportUnit[]
+     */
     public function getStatesTotalTaxes(): array
     {
-        // TODO: Implement getStatesTotalTaxes() method.
+        $data = $this->loadDataFromFile();
+        $result = $this->aggregateStates($data, function (Collection $rows) {
+            return $rows->sum(function ($row) {
+                return $row[4];
+            });
+        });
+
+        return $result;
     }
 
+    /**
+     * @return StateReportUnit[]
+     */
     public function getStatesAverageTaxRate(): array
     {
-        // TODO: Implement getStatesAverageTaxRate() method.
+        $data = $this->loadDataFromFile();
+        $result = $this->aggregateStates($data, function (Collection $rows) {
+            return $rows->average(function ($row) {
+                return $row[5];
+            });
+        });
+
+        return $result;
     }
 
     public function getAverageTaxRate(): float
     {
-        // TODO: Implement getAverageTaxRate() method.
+        $data = collect($this->loadDataFromFile());
+        $result = $data->average(function ($row) {
+            return $row[5];
+        });
+
+        return $result;
     }
 
     public function getTotalTaxes(): float
     {
-        // TODO: Implement getTotalTaxes() method.
+        $data = collect($this->loadDataFromFile());
+        $result = $data->sum(function ($row) {
+            return $row[4];
+        });
+
+        return $result;
     }
 
-    public function loadDataFromFile() : array {
+    protected function loadDataFromFile(): array
+    {
         // load the data file contents and parse the csv format then
         // return the data without the header row as we are only concerned with the data
-        $fileContents = Storage::get(config('reporting.csv.path'));
+        $fileContents = file(storage_path(config('reporting.csv.path')));
         $data = array_map('str_getcsv', $fileContents);
         return array_splice($data, 1);
+    }
+
+    protected function aggregateStates(array $rows, $aggregateFunc)
+    {
+        $data = collect($rows);
+        return $data
+            ->groupBy(function ($row) {
+                return $row[0];
+            })
+            ->map(function ($rows, $state) use ($aggregateFunc) {
+                $aggregate = $aggregateFunc(collect($rows));
+                return new StateReportUnit($state, $rows[0][1], $aggregate);
+            })
+            // we want a plain array structure and we have our own model
+            // so we only fetch the group by operation's values here
+            ->values()
+            ->all();
     }
 }
