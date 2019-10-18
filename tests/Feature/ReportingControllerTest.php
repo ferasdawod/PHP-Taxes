be080489dbf6f165cd012ca4c190619c18667daa
Feras Dawod <feras.da94@gmail.com>
2019-10-18 18:22:22 +0200

    Add feature test for the reporting controller

--- tests/Feature/ReportingControllerTest.php
+++ tests/Feature/ReportingControllerTest.php
@@ -5,17 +5,42 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class ReportingControllerTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testReportingEndpoint()
     {
-        $response = $this->get('/');
+        $response = $this->get('/api/reporting/taxes');
 
         $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'total_taxes',
+            'avg_tax_rate',
+            'states_avg_tax_rate' => [
+                '*' => [
+                    'id',
+                    'name',
+                    'value',
+                ],
+            ],
+            'states_total_taxes' => [
+                '*' => [
+                    'id',
+                    'name',
+                    'value',
+                ],
+            ],
+            'states_avg_taxes' => [
+                '*' => [
+                    'id',
+                    'name',
+                    'value',
+                ],
+            ],
+        ]);
     }
 }
