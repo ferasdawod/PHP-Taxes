95bc5e15d7444ff31f99ecf671cc6d434842eb94
Feras Dawod <feras.da94@gmail.com>
2019-10-18 18:19:42 +0200

    Only log db queries if we're not in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,12 +16,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        \DB::listen(function($sql) {
-            \Log::info($sql->sql);
-            \Log::info($sql->bindings);
-            \Log::info($sql->time);
-        });
+        // Log database queries to the app log if we're not running in production
+        if (config('app.env') !== 'production') {
+            \DB::listen(function($sql) {
+                \Log::info($sql->sql);
+                \Log::info($sql->bindings);
+                \Log::info($sql->time);
+            });
+        }
 
+        // Bind the correct data source for the reporting service
         $reportingImplementation = config('reporting.source') === 'db' ? DbReportingService::class : CsvReportingService::class;
         $this->app->bind(IReportingService::class, $reportingImplementation);
     }
