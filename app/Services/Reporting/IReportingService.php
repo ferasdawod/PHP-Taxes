534cff5772d578607517a084c38ac6ae72570e4d
Feras Dawod <feras.da94@gmail.com>
2019-10-18 18:21:08 +0200

    Document the reporting service functions

--- app/Services/Reporting/IReportingService.php
+++ app/Services/Reporting/IReportingService.php
@@ -7,21 +7,32 @@ use App\ViewModels\Reporting\StateReportUnit;
 interface IReportingService
 {
     /**
+     * Returns the average collected taxes for each state
      * @return StateReportUnit[]
      */
     public function getStatesAverageTaxes() : array ;
 
     /**
+     * Returns the total collected taxes for each state
      * @return StateReportUnit[]
      */
     public function getStatesTotalTaxes() : array;
 
     /**
+     * Returns the average tax rate for each state
      * @return StateReportUnit[]
      */
     public function getStatesAverageTaxRate() : array;
 
+    /**
+     * Returns the average tax rate of the whole country
+     * @return float
+     */
     public function getAverageTaxRate() : float;
 
+    /**
+     * Returns the total collected taxes for the whole country
+     * @return float
+     */
     public function getTotalTaxes() : float;
 }
